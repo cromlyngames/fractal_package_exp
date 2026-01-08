fe780b79ac114921e090e892caa7b437f5addfb3
Patrick Barry <cromlyngames@gmail.com>
2026-01-08 09:01:10 +0000

    create demo shapes function

---
+++ top_level_folder/shape/shapes_1.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan  8 08:48:29 2026
+
+@author: pb910
+"""
+
+
+def shape_maker(length):
+    lorem_ipsum = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    return(lorem_ipsum[0:length])
+
