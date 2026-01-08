482285cc6af4c1f7e5518d52a3e06c29b88b1bbf
Patrick Barry <cromlyngames@gmail.com>
2026-01-08 10:56:33 +0000

    draft deform2fit

---
+++ top_level_folder/deformation/deform2fit.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan  8 09:01:12 2026
+
+@author: pb910
+"""
+
+# take in slice
+# generate expected shape
+# measure fit, deform and repeat until it fits.
+
+# report the solution, and residual error
